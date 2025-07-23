734532ac20ec21430db8cbfa111998af6998adc7
Chris Davis J <jaldi.2@wright.edu>
2025-07-23 10:54:52 -0400

    moved api endpoints to a different file

---
+++ client-app/src/api/endpoints.js
@@ -0,0 +1,5 @@
+export const API_ENDPOINTS = {
+  TRANSCRIBE: `http://localhost:3001/api/transcribe`,
+  INFERENCE: `http://localhost:9394/inference/`,
+  GENERATE_LEARNER_CONTEXT: `http://localhost:9394/onboarding/`,
+};
